14c55846395a7aebb8a54ad5573efef36bc6802e
Leo Volwahsen <volwahsenl@gmail.com>
2022-12-12 22:31:52 +0100

    d

--- my-app/src/views/CardsOverview.js
+++ my-app/src/views/CardsOverview.js
@@ -1,15 +1,30 @@
 import { useEffect, useState } from "react";
 
 import { Container, Row } from "react-bootstrap";
+import { useNavigate, useParams } from "react-router-dom";
 import CardPreview from "../components/CardPreview";
 
 function CardsOverview() {
-    const [card, setCards] = useState([]);
+    const navigate = useNavigate();
+    const { cardId } = useParams();
+    const [card, setCards] = useState({});
+
     useEffect(() => {
-        fetch("http://127.0.0.1:4000/cards")
+        fetch(`http://127.0.0.1:4000/cards/${cardId}`)
             .then((res) => res.json())
             .then((data) => setCards(data));
-    }, []);
+    }, [cardId]);
+
+    // const clickHandler = () => {
+    //     fetch(`http://127.0.0.1:4000/cards/${cardId}`, {
+    //         method: "DELETE",
+    //     }).then((response) => {
+
+    //         if (response.status === 202) {
+    //             navigate("/cards");
+    //         }
+    //     });
+    };
 
     return (
         <>
